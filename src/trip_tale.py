1c820b99cf17c333229fcde10534b22b4d7e803b
tkmsikeda <tkms.ikeda@gmail.com>
2025-02-26 22:22:54 +0900

    src/trip_tale.py 動画とスライドショー動画を結合する処理を追加

--- src/trip_tale.py
+++ src/trip_tale.py
@@ -27,10 +27,12 @@ def get_file_names(directory: str, file_extension: str) -> list:
 
 
 # TODO  write_filepath_to_txtfile関数と共通部分が多いので1個にできないか？
+# 写真からスライドショー動画に変換する対象の写真ファイルを追記
 def write_filepath_to_txtfile_for_image(file_names: list[str]):
     with open("image_files.txt", "w", encoding="utf-8") as file:
         for file_name in file_names:
             file.write(f"file '{file_name}'\nduration 5\n")
+        # ffmpegの仕様で最後のファイルを2度書かないと表示してくれないので、追記。
         file.write(f"file '{file_names[-1]}'")
 
 
@@ -64,6 +66,8 @@ def merge_movies_ffmpeg(shell_command: str):
     subprocess.run(shell_command, shell=True)
 
 
+# TODO リファクタリング：関数名を適切にしたい
+# TODO リファクタリング：ファイル名を変数化したい
 def main():
     """メイン関数"""
     # 対象のディレクトリを指定（例: 現在のディレクトリ）
@@ -88,20 +92,25 @@ def main():
     # -c:a aac： 音声コーデックを AAC に指定
     # image_audio_video.mp4： 出力ファイル名
     # -stream_loop -1: 音声がループされ、動画の長さに合わせられる。
-    # -shortest: により動画の長さでカットされる。
-
-    # 動画ファイル一覧を取得
-    # movie_file_names = get_file_names(directory, "MOV")
-    # write_filepath_to_txtfile_for_movie(movie_file_names)
-    # # 動画ファイルを１個の動画に結合
-    # merge_movies_ffmpeg(shell_command_for_movie_file)
+    # -shortest: 音声が動画の長さでカットされる。
 
     logger.info("画像からスライドショー作成開始")
     logger.info("対象画像ファイルを取得")
     image_file_names = get_file_names(directory, "JPG")
     write_filepath_to_txtfile_for_image(image_file_names)
+    # 音楽なしのスライドショー動画作成
     merge_movies_ffmpeg(shell_command_for_image_file)
+    # スライドショーに音楽を追加した動画に変換
     merge_movies_ffmpeg(shell_command_for_add_auido)
+
+    # 動画ファイル一覧を取得
+    movie_file_names = get_file_names(directory, "MOV")
+    # 動画の最後に写真スライドショー動画を追加すべく、対象に追記
+    movie_file_names.append("./image_audio_video.mp4")
+    # 結合対象の動画ファイルをtxtファイルに記載
+    write_filepath_to_txtfile_for_movie(movie_file_names)
+    # 動画ファイルを１個の動画に結合
+    merge_movies_ffmpeg(shell_command_for_movie_file)
 
 
 if __name__ == "__main__":
