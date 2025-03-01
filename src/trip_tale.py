296a3b85ad034c1ad57d0b76c218afef46e5c94c
tkmsikeda <tkms.ikeda@gmail.com>
2025-03-01 22:48:23 +0900

    trip_tale.py 動画一覧から縦で撮影した動画を見つけ出す処理を追加

--- src/trip_tale.py
+++ src/trip_tale.py
@@ -1,6 +1,9 @@
 import glob
+import json
 import logging
 import subprocess
+
+import cv2
 
 
 logger = logging.getLogger(__name__)
@@ -54,12 +57,86 @@ def scale_and_pad_video():
     return
 
 
+def get_movie_metadata(file_name: str) -> dict:
+    get_metadata_shell_command = (
+        "ffprobe -loglevel quiet -show_streams -print_format json"
+        + f" {file_name} > movie_metadata.json"
+    )
+    merge_movies_ffmpeg(get_metadata_shell_command)
+    movie_metadata_json = open("movie_metadata.json", "r")
+    movie_metadata = json.load(movie_metadata_json)
+
+    return movie_metadata
+
+
+def extract_rotated_video(file_names: list[str]) -> list[int]:
+    """縦で撮影された動画を見つける
+
+    目的: ffmpegだと、横撮影動画と縦撮影動画を結合すると、
+    縦撮影動画が横向きになってしまう。
+    縦動画を見つけ出し、後続処理で、編集する
+    処理: 縦撮影の動画はメタデータのrotetoが0以外なので、rotateで判定する
+
+    Args:
+        file_names(list[str]): 結合対象の動画のファイルpath一覧
+
+    Returns:
+        rotated_video_indexs(list[int]): rotateが0以外の動画が存在するlistのindex
+    """
+    rotated_video_indexs = []
+
+    for i, file_name in enumerate(file_names):
+        movie_metadata = get_movie_metadata(file_name)
+        side_data_list = movie_metadata["streams"][0].get("side_data_list", 0)
+        if side_data_list == 0:
+            continue
+        rotation = side_data_list[0].get("rotation")
+        logger.info(f"{file_name} rotation: {rotation}")
+        if rotation != 0:
+            rotated_video_indexs.append(i)
+
+    return rotated_video_indexs
+
+
+def extract_non_fullhd_videos(file_names: list[str]) -> list[int]:
+    """1920x1080以外の解像度の動画を見つける
+
+    目的：ffmpegにおいて、解像度の異なる複数の動画を結合できないので
+    解像度が1920x1080以外の動画を見つけ出すこと
+
+    Args:
+        file_names(list[str]): 結合対象の動画のファイルpath一覧
+
+    Returns:
+        non_fullhd_video_indexs(list[int]): 1920x1080以外の動画が存在するlistのindex
+    """
+
+    FULLHD_WIDTH_PX = 1920
+    FULLHD_HEIGHT_PX = 1080
+
+    non_fullhd_video_indexs = []
+    for i, file_name in enumerate(file_names):
+        cap = cv2.VideoCapture(file_name)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        if width != FULLHD_WIDTH_PX or height != FULLHD_HEIGHT_PX:
+            non_fullhd_video_indexs.append(i)
+            logger.debug(
+                f"Found Non FullHD : {file_name}"
+                + f", width: {width}, height: {height}, index: {i}"
+            )
+
+    return non_fullhd_video_indexs
+
+
 # moviepyだと問題２個ありなので、ffmpegを利用した。
 # 問題1. 動画を結合するとbitrateが下がる。動画の画質ダウンになりそう。
 #        ->ffmpegコマンドだと下がらない。
 # 問題2. スマートフォンで縦撮影の動画だと、解像度とアスペクト比が変わる。
 # shellのコマンドでffmpegを実行する方式
 # ffmpegライブラリが結局↑を実施していて、使い方も理想形ではないので、自分で書く。
+# TODO 関数名と中身が伴わなくなってきたので、適切な名前に変える
 def merge_movies_ffmpeg(shell_command: str):
     # shell_command = "ffmpeg -f concat -safe 0 -i files.txt -c copy final_video.MOV"
     logger.debug(f"shell実行: {shell_command}")
@@ -68,13 +145,16 @@ def merge_movies_ffmpeg(shell_command: str):
 
 # TODO リファクタリング：関数名を適切にしたい
 # TODO リファクタリング：ファイル名を変数化したい
+# TODO リファクタリング: コマンドをJSONファイルに外だしして読み込む
 def main():
     """メイン関数"""
     # 対象のディレクトリを指定（例: 現在のディレクトリ）
     directory = "/mnt/nas/20500101_自動化テスト用"
+    # 複数の動画ファイルを結合して1個の動画にするshellコマンド
     shell_command_for_movie_file = (
         "ffmpeg -f concat -safe 0 -i movie_files.txt -c copy final_video.MOV"
     )
+    # 複数の画像ファイルを結合してスライドショー動画にするshellコマンド
     shell_command_for_image_file = (
         "ffmpeg -f concat -safe 0 -i image_files.txt"
         + " -vsync vfr -vcodec libx264"
@@ -82,6 +162,7 @@ def main():
         + 'pad=1920:1080:(ow-iw)/2:(oh-ih)/2"'
         + " -pix_fmt yuv420p output.mp4"
     )
+    # 無音のスライドショー動画に音楽を追加するshellコマンド
     shell_command_for_add_auido = (
         "ffmpeg -stream_loop -1 -i audio.mp3 -i output.mp4"
         + " -c:v copy -c:a aac -shortest image_audio_video.mp4"
@@ -94,23 +175,30 @@ def main():
     # -stream_loop -1: 音声がループされ、動画の長さに合わせられる。
     # -shortest: 音声が動画の長さでカットされる。
 
-    logger.info("画像からスライドショー作成開始")
-    logger.info("対象画像ファイルを取得")
-    image_file_names = get_file_names(directory, "JPG")
-    write_filepath_to_txtfile_for_image(image_file_names)
-    # 音楽なしのスライドショー動画作成
-    merge_movies_ffmpeg(shell_command_for_image_file)
-    # スライドショーに音楽を追加した動画に変換
-    merge_movies_ffmpeg(shell_command_for_add_auido)
+    # logger.info("画像からスライドショー作成開始")
+    # logger.info("対象画像ファイルを取得")
+    # image_file_names = get_file_names(directory, "JPG")
+    # write_filepath_to_txtfile_for_image(image_file_names)
+    # # 音楽なしのスライドショー動画作成
+    # merge_movies_ffmpeg(shell_command_for_image_file)
+    # # スライドショーに音楽を追加した動画に変換
+    # merge_movies_ffmpeg(shell_command_for_add_auido)
 
     # 動画ファイル一覧を取得
     movie_file_names = get_file_names(directory, "MOV")
-    # 動画の最後に写真スライドショー動画を追加すべく、対象に追記
-    movie_file_names.append("./image_audio_video.mp4")
+
+    # 縦撮影の動画を見つける
+    rotated_video_indexs = extract_rotated_video(movie_file_names)
+
+    # TODO 1920x1080へ解像度を変換する
+    # TODO ファイルパスに加える
+
+    # # 動画の最後に写真スライドショー動画を追加すべく、対象に追記
+    # movie_file_names.append("./image_audio_video.mp4")
     # 結合対象の動画ファイルをtxtファイルに記載
-    write_filepath_to_txtfile_for_movie(movie_file_names)
-    # 動画ファイルを１個の動画に結合
-    merge_movies_ffmpeg(shell_command_for_movie_file)
+    # write_filepath_to_txtfile_for_movie(movie_file_names)
+    # # 動画ファイルを１個の動画に結合
+    # merge_movies_ffmpeg(shell_command_for_movie_file)
 
 
 if __name__ == "__main__":
