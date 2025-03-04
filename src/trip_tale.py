2b59aed417b0cf93acbc73a716308dfa50bb9ea6
tkmsikeda <tkms.ikeda@gmail.com>
2025-03-04 22:21:32 +0900

    src/trip_tale.py 不要になった縦どり動画用の処理を削除 ffmpeg_command.json 不要になった縦どり動画用のコマンドを削除 issue #6 に対応

--- src/trip_tale.py
+++ src/trip_tale.py
@@ -47,86 +47,6 @@ def write_filepath_to_txtfile_for_movie(file_names: list[str]):
             file.write(f"file '{file_name}'\n")
 
 
-def update_taget_list(
-    movie_file_names: list[str], rotated_video_indexs: list[int]
-) -> list:
-    updated_target_list = movie_file_names.copy()
-    for rotated_video_index in rotated_video_indexs:
-        updated_target_list[rotated_video_index] = (
-            "rotated_" + str(rotated_video_index) + ".MOV"
-        )
-    return updated_target_list
-
-
-# 縦どり動画を変換する関数
-# 変換1: 縦どりの動画を90度回転させる。
-# 変換2: 解像度を1920x1080に変換する。
-# 変換3: スケーリングにより余った横枠を黒枠を追加する。paddingするとも呼ぶ模様。
-def scale_and_pad_video(file_names: list[str], target_indexs) -> None:
-
-    ffmpeg_command_template = FFMPEG_COMMAND["rotate"]
-    # -loglevel quiet : ffmpegの標準出力を抑制
-    # -vf "scale=1920:1080: 解像度を1920x1080に変換
-    # :force_original_aspect_ratio=decrease: アスペクト比を維持
-    # pad=1920:1080:(ow-iw)/2:(oh-ih)/2": 余った横枠へ黒枠を追加する
-    # transposeオプションを指定しなくても、自動的に解釈して、回転してくれる。
-
-    for target_index in target_indexs:
-        ffmpeg_command_formated = ffmpeg_command_template.format(
-            file_name=file_names[target_index],
-            output_name="rotated_" + str(target_index) + ".MOV",
-        )
-        run_shell_command(ffmpeg_command_formated)
-
-    return
-
-
-def get_movie_metadata(file_name: str) -> dict:
-    get_metadata_shell_command = (
-        "ffprobe -loglevel quiet -show_streams -print_format json"
-        + f" {file_name} > movie_metadata.json"
-    )
-    # -loglevel quiet: ffmpegの標準出力を抑制
-    # -show_streams: メタデータを表示する
-    # -print_format json: json形式で出力する
-    # > movie_metadata.json: 出力結果をmovie_metadata.jsonに保存する
-    run_shell_command(get_metadata_shell_command)
-    with open("movie_metadata.json", "r") as movie_metadata_json:
-        movie_metadata = json.load(movie_metadata_json)
-
-    return movie_metadata
-
-
-def extract_rotated_video(file_names: list[str]) -> list[int]:
-    """縦で撮影された動画を見つける
-
-    目的: ffmpegだと、横撮影動画と縦撮影動画を結合すると、
-    縦撮影動画が横向きになってしまう。
-    縦動画を見つけ出し、後続処理で、編集する
-    処理: 縦撮影の動画はメタデータのrotetoが0以外なので、rotateで判定する
-
-    Args:
-        file_names(list[str]): 結合対象の動画のファイルpath一覧
-
-    Returns:
-        rotated_video_indexs(list[int]): rotateが0以外の動画が存在するlistのindex
-    """
-    rotated_video_indexs = []
-
-    for i, file_name in enumerate(file_names):
-        movie_metadata = get_movie_metadata(file_name)
-        side_data_list = movie_metadata["streams"][0].get("side_data_list", 0)
-        # side_data_listがない≒横で撮影した動画なので次のループへ
-        if side_data_list == 0:
-            continue
-        rotation = side_data_list[0].get("rotation")
-        logger.info(f"{file_name} rotation: {rotation}")
-        if rotation != 0:
-            rotated_video_indexs.append(i)
-
-    return rotated_video_indexs
-
-
 # moviepyだと問題２個ありなので、ffmpegを利用した。
 # 問題1. 動画を結合するとbitrateが下がる。動画の画質ダウンになりそう。
 #        ->ffmpegコマンドだと下がらない。
@@ -143,8 +63,15 @@ def format_all_movie(file_names: list[str]) -> list[str]:
 
     動画の種類によって、処理が変わる。ただしコマンドは同一のもの。
     横取り動画の場合: ffmpegによりコーデックされるのみ
-    縦どり動画の場合: ffmpegによりコーデックされる事に加え、解像度を1920x1080に変換し
-    余った横枠に黒枠を追加する
+    縦どり動画の場合: ffmpegによりコーデックされる事に加え、90度回転後に、
+    解像度を1920x1080に変換し、余った横枠に黒枠を追加する
+
+    ffmpegコマンドの詳細
+    -loglevel quiet : ffmpegの標準出力を抑制
+    -vf "scale=1920:1080: 解像度を1920x1080に変換
+    :force_original_aspect_ratio=decrease: アスペクト比を維持
+    pad=1920:1080:(ow-iw)/2:(oh-ih)/2": 余った横枠へ黒枠を追加する
+    transposeオプションを指定しなくても、自動的に解釈して、回転してくれる。
 
     Args:
         file_names(list[str]): 動画ファイルのリスト
@@ -212,14 +139,6 @@ def main():
     movie_file_names = get_file_names(directory, "MOV")
     # 動画のフォーマットを統一する
     formatted_movie_file_names = format_all_movie(movie_file_names)
-
-    # # 縦撮影の動画を見つける
-    # rotated_video_indexs = extract_rotated_video(movie_file_names)
-    # # 縦撮影の動画を変換する。原本動画は変えない。一時ファイルをローカルに配置する。
-    # scale_and_pad_video(movie_file_names, rotated_video_indexs)
-    # # movie_file_namesへ縦どり動画のパスで更新
-    # movie_file_names = update_taget_list(movie_file_names, rotated_video_indexs)
-    # print(movie_file_names)
 
     # # 動画の最後に写真スライドショー動画を追加すべく、対象に追記
     # movie_file_names.append("./image_audio_video.mp4")
